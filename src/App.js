4d3e3a135bb16657c994f92eccdd08df29c18ad1
Lugofr01 <78613296+Lugofr01@users.noreply.github.com>
2022-06-15 09:11:02 -0500

    LINKS

--- src/App.js
+++ src/App.js
@@ -2,15 +2,56 @@ import React, { Component } from "react";
 // import logo from "./logo.svg";
 import Movies from "./components/movies";
 import "./App.css";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 class App extends Component {
   render() {
     return (
       <main className="container">
-        <Movies />
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/users">Users</Link>
+                </li>
+              </ul>
+            </nav>
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/users">
+                <Users />
+              </Route>
+              <Route path="/movies">
+                <Movies />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </main>
     );
   }
+}
+
+
+
+// this ones below emulate compnents and can therefore be imported
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
