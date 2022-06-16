3c3d7cffd1a7f822dd27671125efeb80dcbf90d3
Lugofr01 <78613296+Lugofr01@users.noreply.github.com>
2022-06-16 16:23:41 -0500

    Forms and Validation

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import Customers from "./components/customers";
 import NotFound from "./components/notFound";
 import Rentals from "./components/rentals";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+import LoginForm from "./components/loginForm";
 class App extends Component {
   render() {
     return (
@@ -15,19 +22,26 @@ class App extends Component {
             <nav>
               <ul>
                 <li>
-                  <Link to="/">Home</Link>
+                  <Link to="/">Mezani Vid</Link>
                 </li>
                 <li>
-                  <Link to="/about">About</Link>
+                  <Link to="/movies">Movies</Link>
                 </li>
                 <li>
-                  <Link to="/users">Users</Link>
+                  <Link to="/customers">Customers</Link>
+                </li>
+                <li>
+                  <Link to="/rentals">Rentals</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
                 </li>
               </ul>
             </nav>
             {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
             <Switch>
+              <Route path="/login" component={LoginForm}></Route>
               <Route path="/movies" component={Movies}></Route>
 
               <Route path="/not-found">
@@ -40,6 +54,8 @@ class App extends Component {
               <Route path="/rentals">
                 <Rentals />
               </Route>
+              <Redirect from="/" exact to="/movies" />
+              <Redirect to="/not-found" />
             </Switch>
           </div>
         </Router>
@@ -49,13 +65,5 @@ class App extends Component {
 }
 
 // this ones below emulate compnents and can therefore be imported
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
 
 export default App;
---
+++ src/components/loginForm.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import Input from "./input";
+
+class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" },
+    errors: {},
+  };
+
+  validate = () => {
+    return { username: "Username is required" };
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const errors = this.validate();
+    this.setState({ errors });
+    if (errors) return;
+
+    console.log("submitted");
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
+  render() {
+    const { account } = this.state;
+
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handleChange}
+          />
+          <Input
+            name="password"
+            value={account.password}
+            label="Password"
+            onChange={this.handleChange}
+          />
+          <button className="btn btn-primary btn-small m-4">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
